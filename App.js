12c03fcc9ba0fcb63b5f06209589ec41866b85be
yogieputra8 <yogie.putra8@gmail.com>
2019-01-10 21:34:43 +0700

    add signin

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react"
 import { StyleSheet, Text, View } from "react-native"
 import SignUp from './src/SignUp'
+import SignIn from './src/SignIn'
 
 class App extends Component {
   render() {
-    return <SignUp />
+    return <SignIn />
   }
 }
 
